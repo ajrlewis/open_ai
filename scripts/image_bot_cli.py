2ff12fc4e85ae740e9e9ea12557ae830f9cf8493
ajrlewis <hello@ajrlewis.com>
2024-05-29 11:27:55 +0100

    Searching for available image filenames to save to.

--- scripts/image_bot_cli.py
+++ scripts/image_bot_cli.py
@@ -18,14 +18,19 @@ def main(args: argparse.Namespace):
     filepath = f"{args.data_dir}/image-bot-{args.subject}.json"
     client = openai.OpenAI(api_key=OPEN_AI_API_KEY)
     image_bot = ImageBot(client=client)
-    with open(f"{args.data_dir}/chat-bot-{args.subject}.md") as f:
+    with open(f"{args.data_dir}/image-bot-{args.subject}.md") as f:
         prompt = f.read()
-        image_bot.generate(prompt, style="natural")
-        # image_bot.generate(prompt, style="vivid")
-    filepath = f"data/image-bot-{args.subject}-{i}.png"
-    while os.path.exists(filepath):
-        i += 1
-        filepath = f"data/image-bot-{args.subject}-{i}.png"
+        print(f"{prompt = }")
+        image_bot.generate(prompt, style=args.style)
+
+    # Get image filename and save
+    base_filepath = f"data/image-bot-{args.subject}"
+    for i in range(100):
+        filepath = f"{base_filepath}-{i}.png"
+        if os.path.exists(filepath):
+            continue
+        else:
+            break
     image_bot.write(filepath)
 
 
@@ -40,6 +45,11 @@ if __name__ == "__main__":
         "--subject",
         default="misc",
         help="Subject of the image bot JSON file.",
+    )
+    parser.add_argument(
+        "--style",
+        default="vivid",
+        help="Style of the image; natural or vivid.",
     )
     args = parser.parse_args()
     main(args)
